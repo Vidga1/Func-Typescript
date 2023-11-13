8b9ef503c0fe3e335181c11d54967920c68dec3e
Vladislav <vidga471@gmail.com>
2023-11-14 00:02:18 +0300

    fix curry

--- src/curry.ts
+++ src/curry.ts
@@ -1,46 +1,35 @@
-// Определяем тип для функции с пятью параметрами
-type FuncWithFiveArgs<T1, T2, T3, T4, T5, R> = (
-  a: T1,
-  b: T2,
-  c: T3,
-  d: T4,
-  e: T5,
-) => R;
+type SameLength<T extends unknown[]> = Extract<
+  { [K in keyof T]: unknown },
+  unknown[]
+>;
 
-// Определяем тип для каррированной функции
-type CurriedFunction<T1, T2, T3, T4, T5, R> = (
-  a: T1,
-) => (b: T2) => (c: T3) => (d: T4) => (e: T5) => R;
+type Curry<P extends unknown[], R> = <T extends Partial<P>>(
+  ...args: T
+) => T extends P
+  ? R
+  : P extends [...SameLength<T>, ...infer S]
+  ? S extends unknown[]
+    ? Curry<S, R>
+    : never
+  : never;
 
-export function curry<T1, T2, T3, T4, T5, R>(
-  func: FuncWithFiveArgs<T1, T2, T3, T4, T5, R>,
-): CurriedFunction<T1, T2, T3, T4, T5, R> {
-  return function first(a: T1) {
-    return function second(b: T2) {
-      return function third(c: T3) {
-        return function fourth(d: T4) {
-          return function fifth(e: T5) {
-            return func(a, b, c, d, e);
-          };
-        };
-      };
-    };
-  };
+export function curry<P extends unknown[], R>(
+  fn: (...args: P) => R,
+): Curry<P, R> {
+  return function curried(...args: unknown[]): unknown {
+    if (args.length >= fn.length) {
+      return fn(...(args as P));
+    }
+    return (...args2: unknown[]) => curried(...args, ...args2);
+  } as Curry<P, R>;
 }
 
-// Пример использования
-const func: FuncWithFiveArgs<number, number, number, number, number, number> = (
-  a,
-  b,
-  c,
-  d,
-  e,
-) => a + b + c + d + e;
-const hof: CurriedFunction<number, number, number, number, number, number> =
-  curry(func);
-
-console.log(hof(1)(2)(3)(4)(5)); // 15
-console.log(hof(2)(3)(4)(5)(6)); // 20
-console.log(hof(3)(4)(5)(6)(7)); // 25
-console.log(hof(4)(5)(6)(7)(8)); // 30
+// Example usage:
+const func = (a: number, b: number, c: number, d: number, e: number) =>
+  a + b + c + d + e;
+const hof = curry(func);
+console.log(hof(1, 2, 3, 4, 5)); // 15
+console.log(hof(2, 3, 4)(5, 6)); // 20
+console.log(hof(3, 4)(5, 6)(7)); // 25
+console.log(hof(4)(5)(6)(7, 8)); // 30
 console.log(hof(5)(6)(7)(8)(9)); // 35
